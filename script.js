1fd6fc1f593b015a7c8ade532c45abbe7fe8f762
Kay_H <kayholliday@ymail.com>
2024-03-11 06:22:23 +0000

    Add message

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const instruments = ['tamborine', 'maraca', 'piano', 'triangle'];
 const action = ['march', 'lie down', 'clap hands', 'wiggle your fingers'];
 
 //console.log(nurseryRyhme,instruments,action); testing code
+
+const generateRandomNurseryRyhme = () => {
+    let randomNurseryRhyme = nurseryRyhme[Math.floor(Math.random()*nurseryRyhme.length)];
+    let randominstrument = instruments[Math.floor(Math.random()*instruments.length)];
+    let randomaction = action[Math.floor(Math.random()*action.length)];
+    console.log(`You should sing ${randomNurseryRhyme}`);
+    console.log(`You should use a ${randominstrument} whilst you sing`);
+    console.log(`Remember to ${randomaction} as you sing!`);
+}
+
+generateRandomNurseryRyhme();
