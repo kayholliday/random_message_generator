8a35c5ee377223d6048083879939dc543cb5aeaa
Kay_H <kayholliday@ymail.com>
2024-03-11 05:48:41 +0000

    Add message components

---
+++ script.js
@@ -0,0 +1,5 @@
+const nurseryRyhme = ['Twinkle twinkle little star', 'Grand Old Duke of York', 'Sleeping Bunnies', 'Incey wincey spider', 'Old MacDonald had a farm'];
+const instruments = ['tamborine', 'maraca', 'piano', 'triangle'];
+const action = ['march', 'lie down', 'clap hands', 'wiggle your fingers'];
+
+//console.log(nurseryRyhme,instruments,action); testing code
